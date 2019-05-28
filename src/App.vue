9fbf3de44064d0376d93add8a85b5d216cb27f5a
Danilo Ferreira <danilofe@live.com>
2019-05-28 19:15:05 -0300

    WEB-EVENTS STEP 2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <TestingUiEvents></TestingUiEvents>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import HelloWorld from './components/HelloWorld.vue'
+import TestingUiEvents from './components/TestingUiEvents.vue'
 
 @Component({
   components: {
+    TestingUiEvents,
     HelloWorld
   }
 })
---
+++ src/components/TestingUiEvents.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div>
+    <button v-on:click="testingGet">
+      Test the get
+    </button>
+
+    <div v-for="event in eventsResults" v-bind:key="event">
+      <span>{{event}}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class TestingUiEvents extends Vue {
+  eventsResults: String[] = []
+
+  testingGet = () => {
+    fetch('http://localhost:3000/testing').then(response => response.text()).then(body => this.eventsResults.push(body))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
